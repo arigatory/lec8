740b859e800185c5db74df9f9f713319e8c7864f
Иван Иван <remillius_dies@outlook.com>
2019-11-16 11:35:09 +0300

    Add project files.

---
+++ lec8/NumericalIntegrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec8
+{
+    public delegate double F(double x);
+    class NumericalIntegrator
+    {
+        public double Integrate(F f, double a, double b,int n)
+        {
+            double result = 0;
+            double delta = (b - a) / n;
+            for (int i = 0; i < n; i++)
+            {
+                result += f(a + delta * i) * delta;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ lec8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec8
+{
+    delegate int MathOperation(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation minus = Sub;
+
+            int result = minus(5, 4);
+            Console.WriteLine(result);
+
+            minus = new MathOperation(Program.Add);
+            result = minus(3, 4);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
